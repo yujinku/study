61844c54c7bb49c77969a29e73856f87cb108c77
yujin <yyyjjj1129@daum.net>
2021-10-22 16:59:13 +0900

    study

---
+++ 숙제.js
@@ -0,0 +1,12 @@
+// var , let , const 로 선언해보기 10/22 금요일
+1. name 
+let name = "yujin";
+console.log (name);
+
+2. age
+const age = 27;
+console.log (age);
+
+3. hobby
+var hobby = "watching movies";
+console.log (hobby);
